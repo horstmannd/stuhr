e95497e10ce8ec9ffb5107bf71deecd130250804
horstmannd <dustin.horstmann@gmail.com>
2018-01-21 20:27:34 -0600

    Add anion page

--- js/main.js
+++ js/main.js
@@ -13,8 +13,9 @@ $(document).ready(function(){
 
   // Back., Elastic., Bounce., SlowMo.
   
-  TweenMax.from('.home-hero', 1, {opacity:0});
-  TweenMax.from('.navbar-brand', .5, {top: -50, opacity: 0, ease:Power0.easeOut}); 
+  TweenMax.from('.jumbotron', 1, {opacity:0});
+	TweenMax.from('.navbar-brand', .5, {top: -50, opacity: 0, ease:Power0.easeOut}); 
+	TweenMax.from('.jumbotron img', 1, {bottom: -50, opacity: 0, ease:Power0.easeOut})
 });
 
 
